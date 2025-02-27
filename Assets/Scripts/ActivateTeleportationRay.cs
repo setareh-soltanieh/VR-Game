8748458b81886678cf66ee4e4773e9751deb6f16
Setareh <22ss75@queensu.ca>
2025-02-27 16:53:25 -0500

    Modified the ActivateTeleportationRay so that whenever the user grabs something they cant use the teleportation

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -10,6 +10,12 @@ public class ActivateTeleportationRay : MonoBehaviour
 
     public InputActionProperty leftHandActivate;
     public InputActionProperty rightHandActivate;
+
+    // Using these two inputs I am checking whether the grip button is activated or not, the user can only use the teleportation if 
+    // the grip button are not activated 
+    public InputActionProperty leftHandCancel;
+    public InputActionProperty rightHandCancel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +25,7 @@ public class ActivateTeleportationRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        leftHandTeleportation.SetActive(leftHandActivate.action.ReadValue<float>() > 0.1f);
-        rightHandTeleportation.SetActive(rightHandActivate.action.ReadValue<float>() > 0.1f);
+        leftHandTeleportation.SetActive(leftHandCancel.action.ReadValue<float>() == 0 && leftHandActivate.action.ReadValue<float>() > 0.1f);
+        rightHandTeleportation.SetActive(rightHandCancel.action.ReadValue<float>() == 0 && rightHandActivate.action.ReadValue<float>() > 0.1f);
     }
 }
